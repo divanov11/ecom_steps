8692c26cfec30ed0d4cc62de72670f6a74ec0e70
divanov11 <33843378+divanov11@users.noreply.github.com>
2020-04-13 09:38:23 -0700

    Create urls_part3.py

---
+++ urls_part3.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+	#Leave as empty string for base url
+	path('', views.store, name="store"),
+	path('cart/', views.cart, name="cart"),
+	path('checkout/', views.checkout, name="checkout"),
+
+]
